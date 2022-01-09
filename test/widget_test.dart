43381ee288f3614dda43808195971531cd3fc9a9
Bayu Saputra <babahayoo@gmail.com>
2022-01-09 22:29:22 +0700

    adding internet permission, and fix issue when declare variable as null

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http_request/main.dart';
 
-void wmain() {
+void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
